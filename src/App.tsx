fd22036c828b8be5dd1059b5a07f97ad5a2158b3
MuhammadAman02 <mohammada@hst.ie>
2025-05-31 03:03:17 +0500

    update file

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Bags from "./pages/Bags";
+import Wallets from "./pages/Wallets";
+import Accessories from "./pages/Accessories";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  console.log('App component initialized');
+  console.log('App component initialized with routing');
   
   return (
     <QueryClientProvider client={queryClient}>
@@ -18,6 +23,11 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/bags" element={<Bags />} />
+            <Route path="/wallets" element={<Wallets />} />
+            <Route path="/accessories" element={<Accessories />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
